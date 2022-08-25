b95db27935d0af80f8815a9133726ba10089392d
Hala39 <hala.taleb.39@gmail.com>
2022-08-26 01:50:12 +0300

    technologies skeleton implemented

--- src/app/sections/technologies/technologies.component.ts
+++ src/app/sections/technologies/technologies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-technologies',
@@ -10,6 +10,58 @@ export class TechnologiesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  } 
+
+  @ViewChild('technosRef') technosUlRef!: ElementRef<HTMLUListElement>;
+
+  goRight() {
+    const elementWidth = 25 * this.technosUlRef.nativeElement.offsetWidth / 100;
+    if (this.technosUlRef.nativeElement.scrollLeft + elementWidth < elementWidth * 4)
+      this.technosUlRef.nativeElement.scrollLeft += elementWidth;
+    else return;
   }
+
+  goLeft() {
+    const elementWidth = 25 * this.technosUlRef.nativeElement.offsetWidth / 100;
+    this.technosUlRef.nativeElement.scrollLeft -= elementWidth;
+  }
+
+  technos: {name: string, logo: string}[] = [
+    {
+      name: 'Python',
+      logo: 'assets/images/technologies/python.png'
+    },
+    {
+      name: 'Net',
+      logo: 'assets/images/technologies/python.png'
+    },{
+      name: 'WordPress',
+      logo: 'assets/images/technologies/python.png'
+    },{
+      name: 'Python',
+      logo: 'assets/images/technologies/python.png'
+    },{
+      name: 'Python',
+      logo: 'assets/images/technologies/python.png'
+    },{
+      name: 'Python',
+      logo: 'assets/images/technologies/python.png'
+    },{
+      name: 'Python',
+      logo: 'assets/images/technologies/python.png'
+    },{
+      name: 'Python',
+      logo: 'assets/images/technologies/python.png'
+    },{
+      name: 'Python',
+      logo: 'assets/images/technologies/python.png'
+    },{
+      name: 'Python',
+      logo: 'assets/images/technologies/python.png'
+    },{
+      name: 'Python',
+      logo: 'assets/images/technologies/python.png'
+    }
+  ]
 
 }
