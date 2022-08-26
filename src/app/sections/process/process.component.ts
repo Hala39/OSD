f951d87caa8abd9d35f365491c9af84939c2e61c
Hala39 <hala.taleb.39@gmail.com>
2022-08-26 03:35:43 +0300

    process section skeleton implemented

--- src/app/sections/process/process.component.ts
+++ src/app/sections/process/process.component.ts
@@ -12,4 +12,38 @@ export class ProcessComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  steps: {title: string, content: string, open?: boolean }[] = [
+    {
+      title: 'contact',
+      content: 'you tell us your need',
+    },
+    {
+      title: 'Investigation',
+      content: 'you tell us your need',
+    },
+    {
+      title: 'selection',
+      content: 'you tell us your need',
+    },
+    {
+      title: 'quotation',
+      content: 'you tell us your need',
+    },
+    {
+      title: 'agreement',
+      content: 'you tell us your need',
+    },
+    {
+      title: 'action',
+      content: 'you tell us your need',
+    },
+    {
+      title: 'support',
+      content: 'you tell us your need',
+    }
+  ]
+
+  toggleText($event: any, stepIndex: number) {
+    this.steps[stepIndex].open ? this.steps[stepIndex].open = false : this.steps[stepIndex].open = true;
+  }
 }
